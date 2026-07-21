cd6c04fd2f8c0d89979f6a585c7a5f90f8b86766
Abbashjz2 <clouddata918@gmail.com>
2026-07-22 02:24:30 +0300

    Allow remote install update command

--- services/remoteCommands/RemoteCommandService.js
+++ services/remoteCommands/RemoteCommandService.js
@@ -43,6 +43,7 @@ const SUPPORTED_COMMANDS = Object.freeze([
   'restart_monitor',
   'restart_heartbeat',
   'reset_router_pool',
+  'install_update',
 ]);
 
 function nowIso() { return new Date().toISOString(); }
